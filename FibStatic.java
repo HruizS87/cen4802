7d53c6322a5d8e7a5635de1962cb0a94cd49eb56
HruizS87 <91905027+HruizS87@users.noreply.github.com>
2022-09-04 19:21:52 -0400

    Documented FibStatic.java
    Method's purpose description has been added along with @param, @authour and @result.

--- FibStatic.java
+++ FibStatic.java
@@ -1,6 +1,13 @@
 import java.util.*;
 class FibStatic extends Thread {  // Recursive Fibonacci
-
+    /**
+     * @author Hector E Ruiz Silva
+     * The following is a static method that will execute the value of any given fibonnaci
+     * sequence number (n) when entered.
+     * @param n integer that will be used to determine the value of the number entered
+     * @return the result takes the number (n) and adds them resulting in the fibonnaci
+     *         sequence value.
+     */
     public static int Fibonacci(int n) {
         if(n==0) return 0;
         if(n==1) return 1;
